15215a3a757c35026996a897a68b38fe7b659c45
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2018-01-07 23:41:55 -0200

    Aula 1 - Instação do Node.js

--- casadocodigo/app.js
+++ casadocodigo/app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.set('view engine','ejs');
+
 app.get('/produtos', function (req, res) {
-    res.send("<html><body><h1>Listando os Produtos</h1></body></html>");
+    res.render("produtos/lista");
 
 });
 
